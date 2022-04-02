db80bf6ccc9a2d093eeed2867a944771e335ac6c
B3VR <73071619+B3VR@users.noreply.github.com>
2022-04-02 19:10:23 +0200

    pierwszy commit

---
+++ IP_Projekt/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IP_Projekt.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
