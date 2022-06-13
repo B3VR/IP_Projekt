c46df57f5eddae6bd4ce4ac8aa84e7dd3ae27987
Guma02 <104900246+Guma02@users.noreply.github.com>
2022-06-13 21:23:26 +0200

    Rejestracja
    Pierwszy widok rejestracji

---
+++ IP_Projekt/Views/Home/Rejestracja.cshtml.cs
@@ -0,0 +1,15 @@
+﻿using IP_Projekt.Views.Home.ViewModels;
+namespace IP_Projekt.Views.Home
+{
+    public class Rejestracja
+    {
+        public RejestracjaViewModel Re_Model { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+
+
+    }
+}
--- IP_Projekt/Views/Home/ViewModels/RejestracjaViewModel.cs
+++ IP_Projekt/Views/Home/ViewModels/RejestracjaViewModel.cs
@@ -1,6 +1,23 @@
-﻿namespace IP_Projekt.Views.Home.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace IP_Projekt.Views.Home.ViewModels
 {
     public class RejestracjaViewModel
     {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string haslo { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(haslo), ErrorMessage = "Przy potwierdzaniu wpisz takie samo hasło jak w polu powyżej")]
+        public string potwierdz_haslo { get; set; }
+
+
     }
 }
