42ef13d8b3e0bea3d70b23774286001662ac8cd3
B3VR <73071619+B3VR@users.noreply.github.com>
2022-06-14 23:35:33 +0200

    Pulpit i okno wizyty

---
+++ IP_Projekt/Controllers/Wizyta/WizytaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IP_Projekt.Controllers.NewFolder
+{
+    public class WizytaController : Controller
+    {
+        public IActionResult Wizyta()
+        {
+            return View();
+        }
+    }
+}
